38d9a1e8cb6ac1df2e6468a6eacd822626ea3880
Bishon Bopanna <bbopanna@mmusketeers.com>
2018-03-24 14:19:43 -0400

    Move Application.java to io/github/bbop as per Spring recommendation

--- src/main/java/io/github/bbop/Application.java
+++ src/main/java/io/github/bbop/Application.java
@@ -1,11 +1,10 @@
-package io.github.bbop.hwapp;
+package io.github.bbop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "io.github.bbop")
 public class Application {
 
 	public static void main(String[] args) {
